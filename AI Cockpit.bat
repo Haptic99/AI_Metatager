36c1de241d1d557e7b72c4c27510f39f814d0c87
Haptic99 <daniel199898gmail.com>
2026-08-23 09:03:01 +0200

    Clean up starters and use python.exe with invisible VBS

--- AI Cockpit.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0\Jellyfin_AI_Cockpit"
-start "" "C:\Program Files\Python313\pythonw.exe" Cockpit_V2.py
--- AI_Cockpit_Starten.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-"C:\Program Files\Python313\pythonw.exe" Cockpit_V2.py
